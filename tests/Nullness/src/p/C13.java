f6c467ab9cc57259456002600fb47771c8a7e522
Amarin Phaosawasdi <amarinph@amarinph.com>
2014-01-31 10:44:49 -0600

    Simplified C13 in Nullness test.

--- tests/Nullness/src/p/C13.java
+++ tests/Nullness/src/p/C13.java
@@ -2,22 +2,20 @@ package p;
 
 // This tests type qualifier inference for the SingleVariableDeclaration case.
 // It propagates @Nullable from a method to another method it overrides.
-public class C13 {
-	abstract class C14 {
-		abstract void c14m1(Object c14o1);
+abstract class C13 {
+	abstract void c13m1(Object c13o1);
+}
+
+class C14 extends C13 {
+	@Override
+	void c13m1(Object c14o1) {
 	}
+}
 
-	class C15 extends C14 {
-		@Override
-		void c14m1(Object c15o1) {
-		}
-	}
+class C15 {
+	C13 c15f1;
 
-	class C16 {
-		C14 c16f1;
-
-		void c16m2() {
-			c16f1.c14m1(null);
-		}
+	void c16m2() {
+		c15f1.c13m1(null);
 	}
 }
