b74a6e7e2345964f37757ea41267f2e1ae83320a
Amarin Phaosawasdi <amarinph@amarinph.com>
2014-07-10 16:50:11 -0500

    Added a test for changing method return types.
    issue #67

---
+++ tests/Nullness/src/p/C19.java
@@ -0,0 +1,37 @@
+package p;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Test that the Cascasde overrides only the qualifiers of the return type of
+ * the methods not the annotations of a method declaration.
+ */
+public class C19 {
+
+	Object c19m1() {
+		return null;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	String c19m2() {
+		return null;
+	}
+
+	@NonNull
+	Integer c19m3() {
+		return null;
+	}
+
+	@NonNull
+	@SuppressWarnings("rawtypes")
+	Object c19m4() {
+		return null;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@NonNull
+	Object c19m5() {
+		return null;
+	}
+
+}
